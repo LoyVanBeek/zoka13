abdc2f196b9b9329b841c569568f5e778d9d1023
Loy van Beek <loy.vanbeek@gmail.com>
2013-08-03 18:24:16 +0200

    Basic version working, need to check diff in serial input more closely and calc size better

--- shoesizer.py
+++ shoesizer.py
@@ -2,26 +2,40 @@
 
 import serial
 import os
+from math import cos, sqrt, radians
+
+BASE_ANGLE = 19
+arm = 34.0
+BASE_DIST = 11.0
+
+def calc_size(angle):
+    angle += BASE_ANGLE
+
+    length = sqrt(arm**2 + arm**2 - 2*arm*arm*cos(radians(angle))) #c = sqrt(a^2 + b^2 - 2*a*b*cos(angle))
+    size = (3.0/2) * (length + 2) #http://en.wikipedia.org/wiki/Shoe_size#Europe
+    return size
+
 
 ser = serial.Serial("/dev/ttyUSB1")  # open first serial port
 print ser.portstr       # check which port was really used
-
 try:
     line = ""
     while True:
         try:
             newLine = ser.readline()
-            print newLine
-            if newLine != line:
+            if newLine[3:] != line[3:]:
+                print newLine
                 line = newLine
                 if "ObjectHit" in line:
                     parts = line.split(" ")
                     angleStr = parts[0]
                     angle = int(angleStr)
+                    size = int(calc_size(angle))
+                    text = "'your size is {0}'".format(size)
+                    print text
+                    os.system("echo '{0}' | festival --tts".format(text))
 
-                    os.system("echo 'your size is {0}' | festival --tts".format(angle))
-
-                    ser.write("start\n")
+                    #ser.write("start\n")
         except KeyboardInterrupt:
             break
 finally:
